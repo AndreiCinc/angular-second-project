141b66128071e841021557ded0e22e58a669914d
AndreiCinc <andrei.cinc9@gmail.com>
2020-09-24 18:22:25 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-xar9pp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-xar9pp)
---
+++ src/app/products.ts
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    color: 'black',
+    size: 'XL'
+  },
+  {
+    name: 'Phone Mini',
+    price: 799,
+    description: 'A great phone with one of the best cameras',
+    color: 'red',
+    size: 'M'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    color: 'white',
+    size: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
